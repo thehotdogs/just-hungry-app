59dcbee88b8d91fed3427acd55f2205d8edc9243
Vincentius Roger Kuswara <vincentiusrkuswara@gmail.com>
2023-03-08 14:47:32 +0800

    Add sign in and login flow, integrate with firestore

--- app/src/main/java/com/example/just_hungry/SignInActivity.java
+++ app/src/main/java/com/example/just_hungry/SignInActivity.java
@@ -20,23 +20,21 @@ import com.google.android.material.textfield.TextInputLayout;
 import com.google.firebase.FirebaseApp;
 import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.FirebaseFirestore;
-import com.google.firebase.firestore.QuerySnapshot;
 
 import java.util.HashMap;
 import java.util.Map;
 
-public class LoginActivity extends AppCompatActivity {
+public class SignInActivity extends AppCompatActivity {
     FirebaseFirestore db = FirebaseFirestore.getInstance();
     private TextInputLayout emailField;
     private TextInputLayout passwordField;
-    private Button loginButton;
-    public boolean isPasswordCorrect;
+    private Button signInButton;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.login_page);
+        setContentView(R.layout.sign_in_page);
 
         // Initialize Firebase
         FirebaseApp.initializeApp(this);
@@ -46,12 +44,12 @@ public class LoginActivity extends AppCompatActivity {
         // getting the email and password ELement
         emailField = (TextInputLayout) findViewById(R.id.emailField);
         passwordField = (TextInputLayout) findViewById(R.id.passwordField);
-        loginButton = (Button) findViewById(R.id.loginButton);
-        TextView donthaveAccountText = findViewById(R.id.donthaveaccount);
+        signInButton = (Button) findViewById(R.id.signInButton);
+        TextView haveAccountText = findViewById(R.id.haveaccount);
         // getting the email and password from the text fields
         String email = emailField.getEditText().getText().toString();
         String password = passwordField.getEditText().getText().toString();
-        Log.d("login",email+ password);
+        Log.d("signIn",email+ password);
 
         // Add logic to check if the username and password are correct
         SharedPreferences preferences = getSharedPreferences("preferences", MODE_PRIVATE);
@@ -61,13 +59,13 @@ public class LoginActivity extends AppCompatActivity {
 //            startHomeActivity();
 //        }
 
-        loginButton.setOnClickListener(new View.OnClickListener() {
+        signInButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 String email = emailField.getEditText().getText().toString();
                 String password = passwordField.getEditText().getText().toString();
 
-                Log.d("INPUTTED:", email + password);
+                Log.d("INPUTTED:", email+ password);
                 // Do logic to check if the email and password are correct
                 if (TextUtils.isEmpty(email)) {
                     emailField.setError("Please enter email");
@@ -99,65 +97,46 @@ public class LoginActivity extends AppCompatActivity {
                 user.put("email", email);
                 user.put("password", password);
 
-
-                db.collection("users")
-                        .whereEqualTo("email", email)
-                        .whereEqualTo("password", password)
-                        .get()
-                        .addOnSuccessListener(new OnSuccessListener<QuerySnapshot>() {
-
+                db.collection("users").add(user)
+                        .addOnSuccessListener(new OnSuccessListener<DocumentReference>() {
                             @Override
-                            public void onSuccess(QuerySnapshot queryDocumentSnapshots) {
-                                System.out.println("QuerySnapshot: " + queryDocumentSnapshots);
-                                if (queryDocumentSnapshots.size() > 0) {
-                                    // Password matches
-
-                                    Toast.makeText(LoginActivity.this, "Password matches", Toast.LENGTH_SHORT).show();
-                                    handleSuccessfulLogin();
-
-                                } else {
-                                    // Password does not match
-                                    isPasswordCorrect = false;
-                                    Toast.makeText(LoginActivity.this, "Password does not match", Toast.LENGTH_SHORT).show();
-
-                                    return;
-                                }
+                            public void onSuccess(DocumentReference documentReference) {
+                                    Toast.makeText(SignInActivity.this, "User added to Firestore", Toast.LENGTH_SHORT).show();
                             }
                         })
                         .addOnFailureListener(new OnFailureListener() {
                             @Override
                             public void onFailure(@NonNull Exception e) {
-                                Toast.makeText(LoginActivity.this, "Error querying Firestore", Toast.LENGTH_SHORT).show();
+                                Toast.makeText(SignInActivity.this, "Error adding user to Firestore", Toast.LENGTH_SHORT).show();
                             }
-                        }); }
-
-                private void handleSuccessfulLogin(){
-                    Log.d("login",email+ password);
-                    System.out.println("Email: " + email);
-                    System.out.println("Password: " + password);
+                        });
 
 
-                    // If the username and password are correct, start the home activity and
-                    // set the "logged_in" preference to true
-                    editor.putBoolean("logged_in", true);
-                    editor.apply();
-                    startHomeActivity();
-                }
+                Log.d("signIn",email+ password);
+                System.out.println("Email: " + email);
+                System.out.println("Password: " + password);
 
 
+                // If the username and password are correct, start the home activity and
+                // set the "logged_in" preference to true
+                editor.putBoolean("logged_in", true);
+                editor.apply();
+                startHomeActivity();
+            }
         });
-        donthaveAccountText.setOnClickListener(new View.OnClickListener() {
+        haveAccountText.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 // Redirect to sign-in page
-                Intent intent = new Intent(LoginActivity.this, SignInActivity.class);
+                Intent intent = new Intent(SignInActivity.this, LoginActivity.class);
                 startActivity(intent);
             }
         });
+
     }
 
     private void startHomeActivity() {
-        Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+        Intent intent = new Intent(SignInActivity.this, MainActivity.class);
         startActivity(intent);
         finish();
     }
